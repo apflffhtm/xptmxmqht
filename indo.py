bb698284395576c3944094969c6fbf69214d2136
apflffhtm <77885346+apflffhtm@users.noreply.github.com>
2021-01-24 18:57:23 +0900

    Update indo.py

--- indo.py
+++ indo.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 client = discord.Client()
 
@@ -29,5 +30,5 @@ async def on_message(message):
 
 
 
-
-client.run("ODAyNTE5MzA0NDIzNDczMTYy.YAwaWA.Z95Km-KP35z_ornWvor9EqdvL4Y")
+access_token = os.environ["BOT_TOKEN"]
+client.run("access_token")
