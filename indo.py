d25bc70a7ff7f327ac394a6c6063c0b695f120d6
apflffhtm <77885346+apflffhtm@users.noreply.github.com>
2021-01-24 18:53:51 +0900

    Add files via upload

---
+++ indo.py
@@ -0,0 +1,33 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("켜짐")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!서버온"):
+        await message.channel.send("@everyone 서버온입니다 다이렉트주소:connect cfx.re/join/oja5yy")
+    if message.content.startswith("!서버오프"):
+        await message.channel.send("@everyone 서버오프라인입니다")
+    if message.content.startswith("!서버리붓"):
+        await message.channel.send("@everyone 서버가 리붓중입니다 잠시만 기달려주세요")
+    if message.content.startswith("!서버점검"):
+        await message.channel.send("@everyone 서버가 점검중입니다 잠시만 기달려주세요")
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.run("ODAyNTE5MzA0NDIzNDczMTYy.YAwaWA.Z95Km-KP35z_ornWvor9EqdvL4Y")
